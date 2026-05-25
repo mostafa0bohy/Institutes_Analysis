04c18c933a49eaf1dd976abf81988fc028c385b7
Mostafa EL-Bohy <96264737+mostafa0bohy@users.noreply.github.com>
2026-05-25 07:56:19 +0300

    Update chatgpt_python.py

--- chatgpt_python.py
+++ chatgpt_python.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 @st.cache_data(ttl=300)
 def load_data():
 
-    url = "PUT_YOUR_PUBLISHED_CSV_LINK_HERE"
+    url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRmiO4XN9kssEddDdU8TuKtXOypsisNKiKejQ-DCDqcgmox6s7DV0zRJ6mxpLqpBA5XQr4JMgFE11_o/pub?output=tsv"
 
     df = pd.read_csv(url)
 
